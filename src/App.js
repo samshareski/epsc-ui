d9232a57df69e9e18e786e27b041c2b708fbf867
Sam Shareski <samshareski@gmail.com>
2017-05-24 16:52:57 -0400

    Make App comp a stateless comp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/header/Header.js'
 import Content from './components/content/Content.js'
 
-class App extends Component {
-  render () {
-    return (
-      <Router>
-        <div>
-          <Header />
-          <hr />
-          <Route path='/:path?' component={Content} />
-        </div>
-      </Router>
-    )
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <hr />
+      <Route path='/:path?' component={Content} />
+    </div>
+  </Router>
+)
 
 export default App
