f954a4c0a367069cbe0df3ef0ab8c055f74cd76d
Sam Shareski <samshareski@gmail.com>
2017-05-28 21:22:00 -0400

    Change home link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from './assets/logo.svg'
+import HomeLink from './HomeLink.js'
 import './Header.css'
-
-const style = {
-  width: 6.25 + 'rem'
-}
 
 const Header = () => (
   <div className='header'>
+    <HomeLink to='/' />
     <Link className='header__link' to='about'>About</Link>
     <Link className='header__link' to='past'>Past</Link>
-    <Link style={style} to='/'>
-      <img src={logo} alt='logo' />
-    </Link>
     <Link className='header__link' to='faq'>F.A.Q.</Link>
     <Link className='header__link' to='future'>Future</Link>
   </div>
---
+++ src/components/header/HomeLink.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from './assets/logo.svg'
+import './HomeLink.css'
+
+const title = 'Exceptional Persons\nSupper Club'
+
+const HomeLink = ({ to }) => (
+  <Link className='header__home-link' to={to}>
+    <img className='header__home-logo' src={logo} alt='logo' />
+    <pre className='header__home-title'>{title}</pre>
+  </Link>
+)
+
+export default HomeLink
