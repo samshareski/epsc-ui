fe17ccd38751014e1b0303795280d9b1888b5ad0
Sam Shareski <samshareski@gmail.com>
2017-05-30 22:42:55 -0400

    Fill in the rest of CTA piece

--- src/components/call-to-action/CallToAction.js
+++ src/components/call-to-action/CallToAction.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import table from './assets/dinner-table.svg'
 import './CallToAction.css'
 
 const CallToAction = () => (
-  <div>
+  <div className='cta'>
     <h1 className='cta-title'>Join Us for Dinner</h1>
+    <h2 className='cta-body'>We promise good food and even better company.</h2>
+    <img className='cta-table' src={table} alt='Dinner Table' />
+    <Link className='cta-button' to='/'>Come To Dinner</Link>
   </div>
 )
 
