af2ac5605792a4332e610b93a5f681f4b43d1d1f
Sam Shareski <samshareski@gmail.com>
2017-05-30 22:09:25 -0400

    Add cta component

---
+++ src/components/call-to-action/CallToAction.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CallToAction.css'
+
+const CallToAction = () => (
+  <div>
+    <h1 className='cta-title'>Join Us for Dinner</h1>
+  </div>
+)
+
+export default CallToAction
