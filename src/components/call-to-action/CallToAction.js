3d404c31192715556cd39b105af1fbfeade93f9c
Sam Shareski <samshareski@gmail.com>
2017-06-01 23:25:52 -0400

    Add future page
    Refactor call to action component for re-use

--- src/components/call-to-action/CallToAction.js
+++ src/components/call-to-action/CallToAction.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import table from './assets/dinner-table.svg'
 import './CallToAction.css'
 
-const CallToAction = () => (
+const CallToAction = ({ title, body, table, linkText, linkTo }) => (
   <div className='cta'>
-    <h1 className='title'>Join Us for Dinner</h1>
-    <h2 className='cta-body'>We promise good food and even better company.</h2>
+    <h1 className='title'>{title}</h1>
+    <h2 className='cta-body'>{body}</h2>
     <img className='cta-table' src={table} alt='Dinner Table' />
-    <Link className='cta-button' to='/'>Come To Dinner</Link>
+    <Link className='cta-button' to={linkTo}>{linkText}</Link>
   </div>
 )
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,10 +2,19 @@ import React from 'react'
 import CallToAction from '../call-to-action/CallToAction.js'
 import Blurbs from '../blurbs/Blurbs.js'
 import Divider from '../divider/Divider.js'
+import table from './assets/dinner-table.svg'
+
+const cta = {
+  title: 'Join Us for Dinner',
+  body: 'Good food and even better company.',
+  table: table,
+  linkText: 'Come To Dinner',
+  linkTo: 'future'
+}
 
 const Home = () => (
   <div>
-    <CallToAction />
+    <CallToAction {...cta} />
     <Divider />
     <Blurbs />
     <Divider />
