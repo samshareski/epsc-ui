f8dbc046f3a6b19b306a6b230ef557dd1c39a471
Sam Shareski <samshareski@gmail.com>
2017-06-03 12:14:28 -0400

    Change main routing rule

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(
   express.static(path.join(__dirname, '..', 'build', 'assets'))
 )
 
-app.use('*', universalLoader)
+app.use('/', universalLoader)
 
 const port = process.env.PORT || 3000
 
