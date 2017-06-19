8c07065e3e5622009ed99459b6c5a7c7b1664c20
Sam Shareski <samshareski@gmail.com>
2017-06-18 21:41:16 -0400

    Create a rotated background for each blurb

--- src/components/blurbs/Blurb.js
+++ src/components/blurbs/Blurb.js
@@ -5,6 +5,7 @@ const Blurb = ({ face, name, blurb }) => (
   <div className='blurb'>
     <img className='blurb__image' src={face} alt={name} />
     <h2 className='blurb__name'>{name}</h2>
+    <div className='blurb__background' />
     <div className='blurb__box'>
       <p className='blurb__blurb'>{blurb}</p>
     </div>
